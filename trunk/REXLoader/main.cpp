9f0b656ad27a6213137e72f2b48eb2d6055419a7
ra9oaj@gmail.com <ra9oaj@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2011-06-19 23:39:31 +0000

    Промежуточная фиксация при разработке GUI

--- trunk/REXLoader/main.cpp
+++ trunk/REXLoader/main.cpp
@@ -1,10 +1,61 @@
 #include <QtGui/QApplication>
 #include "rexwindow.h"
+#include <QDebug>
+
+void checkDatabase()
+{
+    QString homedir = QDir::homePath();
+    QDir homeapp;
+    QString dbname;
+    homeapp.cd(homedir);
+
+    {
+        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+        dbname = db.connectionName();
+        if(!homeapp.exists(homedir+"/.rexloader"))
+            homeapp.mkpath(homedir+"/.rexloader");
+        homeapp.cd(homedir+"/.rexloader");
+
+        bool dbfile = true;
+        if(!QFile::exists(homeapp.path()+"/tasks.db"))dbfile = false;
+        db.setDatabaseName(homeapp.path()+"/tasks.db");
+        db.open();
+        if(!dbfile)
+        {
+            QSqlQuery *qr = new QSqlQuery;
+            bool flag = qr->exec("CREATE TABLE tasks ("
+                "id INTEGER PRIMARY KEY,"
+                "url TEXT,"
+                "datecreate TEXT,"
+                "filename TEXT,"
+                "currentsize TEXT,"
+                "totalsize TEXT,"
+                "downtime TEXT,"
+                "lasterror TEXT,"
+                "mime TEXT,"
+                "categoryid TEXT);");
+
+            if(!flag)
+            {
+                qDebug()<<qr->lastError();
+            }
+            delete(qr);
+        }
+        db.close();
+    }
+    QSqlDatabase::removeDatabase(dbname);
+}
+
+
 
 int main(int argc, char *argv[])
 {
+    checkDatabase();
+
     QApplication a(argc, argv);
+
     REXWindow w;
+    QTimer::singleShot(10,&w,SLOT(close()));
     w.show();
 
     return a.exec();
