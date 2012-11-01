63d2d1811fc4ed43c3ca2e54ddc1e54d933a126c
ra9oaj@gmail.com <ra9oaj@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2012-11-01 14:15:09 +0000

    Исправления: 1. В плагине HttpLoader исправлена ошибка в определении имени файла - в конце имени файла оставалась финализирующие кавычки. Теперь они отслеживаются и удаляются. 2. Исправлены файлы перевода.
    Доработки:
    1. Начата реализация импорта URL из файла: 
       - работает поиск URL в файлах;
       - доработаны функциональные кнопки выделения в диалоге импорта.

--- trunk/REXLoader/importdialog.cpp
+++ trunk/REXLoader/importdialog.cpp
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "importdialog.h"
 #include "ui_importdialog.h"
 
-ImportDialog::ImportDialog(const QString &dir, QWidget *parent) :
+ImportDialog::ImportDialog(const QStringList &files, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ImportDialog)
 {
@@ -27,10 +27,17 @@ ImportDialog::ImportDialog(const QString &dir, QWidget *parent) :
 
     mydb = QSqlDatabase::database();
 
+    connect(ui->selectAllButton,SIGNAL(released()),this,SLOT(selectAll()));
+    connect(ui->deselectAllButton,SIGNAL(released()),this,SLOT(deselectAll()));
+    connect(ui->invertSelectionButton,SIGNAL(released()),this,SLOT(invertSelection()));
+    connect(ui->categoryBox,SIGNAL(currentIndexChanged(int)),this,SLOT(locationSelected(int)));
+
+    imp_files = files;
+    fndurl = 0;
     initialize();
 }
 
-ImportDialog::ImportDialog(const QString &dir, QSqlDatabase &db_, QWidget *parent) :
+ImportDialog::ImportDialog(const QStringList &files, QSqlDatabase &db_, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ImportDialog)
 {
@@ -38,12 +45,20 @@ ImportDialog::ImportDialog(const QString &dir, QSqlDatabase &db_, QWidget *paren
 
     mydb = db_;
 
+    imp_files = files;
     initialize();
 }
 
 ImportDialog::~ImportDialog()
 {
     delete ui;
+}
+
+void ImportDialog::setDownDir(const QString &dir)
+{
+    downDir = dir;
+    ui->locationEdit->setText(dir);
+    loadDatabaseData();
 }
 
 void ImportDialog::loadDatabaseData()
@@ -56,11 +71,32 @@ void ImportDialog::loadDatabaseData()
     {
         QString cattitle;
         if(qr.value(1).toString() == "#downloads")continue;
-        else if(qr.value(1).toString() == "#archives"){cattitle = tr("Архивы"); dirs.insert(qr.value(0).toInt(),downDir+"/"+cattitle);}
-        else if(qr.value(1).toString() == "#apps"){cattitle = tr("Приложения"); dirs.insert(qr.value(0).toInt(),downDir+"/"+cattitle);}
-        else if(qr.value(1).toString() == "#audio"){cattitle = tr("Аудио"); dirs.insert(qr.value(0).toInt(),downDir+"/"+cattitle);}
-        else if(qr.value(1).toString() == "#video"){cattitle = tr("Видео"); dirs.insert(qr.value(0).toInt(),downDir+"/"+cattitle);}
-        else if(qr.value(1).toString() == "#other"){cattitle = tr("Другое"); otherId = qr.value(0).toInt();dirs.insert(qr.value(0).toInt(),downDir);}
+        else if(qr.value(1).toString() == "#archives")
+        {
+            cattitle = tr("Архивы");
+            dirs.insert(qr.value(0).toInt(),(qr.value(2).toString() != "" ? qr.value(2).toString() : downDir)+"/"+cattitle);
+        }
+        else if(qr.value(1).toString() == "#apps")
+        {
+            cattitle = tr("Приложения");
+            dirs.insert(qr.value(0).toInt(),(qr.value(2).toString() != "" ? qr.value(2).toString() : downDir)+"/"+cattitle);
+        }
+        else if(qr.value(1).toString() == "#audio")
+        {
+            cattitle = tr("Аудио");
+            dirs.insert(qr.value(0).toInt(),(qr.value(2).toString() != "" ? qr.value(2).toString() : downDir)+"/"+cattitle);
+        }
+        else if(qr.value(1).toString() == "#video")
+        {
+            cattitle = tr("Видео");
+            dirs.insert(qr.value(0).toInt(),(qr.value(2).toString() != "" ? qr.value(2).toString() : downDir)+"/"+cattitle);
+        }
+        else if(qr.value(1).toString() == "#other")
+        {
+            cattitle = tr("Другое");
+            otherId = qr.value(0).toInt();
+            dirs.insert(qr.value(0).toInt(),(qr.value(2).toString() != "" ? qr.value(2).toString() : downDir));
+        }
         else {cattitle = qr.value(1).toString(); dirs.insert(qr.value(0).toInt(),qr.value(2).toString());}
 
         ui->categoryBox->addItem(cattitle, qr.value(0).toInt());
@@ -68,8 +104,65 @@ void ImportDialog::loadDatabaseData()
     ui->categoryBox->setCurrentIndex(ui->categoryBox->findData(QVariant(otherId)));
 }
 
+void ImportDialog::selectAll()
+{
+    QTableWidgetSelectionRange rng(0,0,ui->tableWidget->rowCount()-1,0);
+    ui->tableWidget->setRangeSelected(rng,true);
+}
+
+void ImportDialog::deselectAll()
+{
+    QTableWidgetSelectionRange rng(0,0,ui->tableWidget->rowCount()-1,0);
+    ui->tableWidget->setRangeSelected(rng,false);
+}
+
+void ImportDialog::invertSelection()
+{
+    QList<QTableWidgetSelectionRange> rngs = ui->tableWidget->selectedRanges();
+    selectAll();
+    QTableWidgetSelectionRange rng;
+    foreach(rng,rngs)
+        ui->tableWidget->setRangeSelected(rng,false);
+}
+
+void ImportDialog::locationSelected(int id)
+{
+    ui->locationEdit->setText(dirs.value(ui->categoryBox->itemData(id).toInt()));
+}
+
+void ImportDialog::import()
+{
+    ImportMaster *importmaster = new ImportMaster(this);
+    connect(importmaster,SIGNAL(finished()),importmaster,SLOT(deleteLater()));
+    connect(importmaster,SIGNAL(foundString(QString)),this,SLOT(addUrl(QString)));
+    connect(importmaster,SIGNAL(totalStringLoaded(qint64)),this,SLOT(readNewLine(qint64)));
+    importmaster->import(imp_files);
+}
+
+void ImportDialog::addUrl(const QString &url)
+{
+    ui->tableWidget->setRowCount(ui->tableWidget->rowCount()+1);
+    QTableWidgetItem *itm = new QTableWidgetItem(url);
+    itm->setData(Qt::ToolTipRole,url);
+    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,0,itm);
+    ui->foundURLs->setText(QString::number(++fndurl));
+}
+
+void ImportDialog::readNewLine(qint64 cnt)
+{
+    ui->totalStrings->setText(QString::number(cnt));
+}
+
 void ImportDialog::initialize()
 {
     if(!parent())setWindowIcon(QIcon(":/appimages/trayicon.png"));
     setAttribute(Qt::WA_DeleteOnClose);
+    fndurl = 0;
+    ui->tableWidget->horizontalHeader()->hide();
+    ui->tableWidget->verticalHeader()->hide();
+    ui->tableWidget->setHorizontalScrollMode(QAbstractItemView::ScrollPerPixel);
+    ui->tableWidget->horizontalHeader()->setResizeMode(QHeaderView::ResizeToContents);
+    ui->tableWidget->verticalHeader()->setResizeMode(QHeaderView::ResizeToContents);
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->hideColumn(1);
 }
