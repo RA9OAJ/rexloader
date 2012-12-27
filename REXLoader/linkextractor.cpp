3a8cbcde059052f57a96d7947fd4f64f40b0f2b6
vedun.mail@gmail.com <vedun.mail@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2012-12-27 12:39:45 +0000

    issue 13 задействовал webkit для извлечения ссылок из html страницы.

--- REXLoader/linkextractor.cpp
+++ REXLoader/linkextractor.cpp
@@ -1,10 +1,19 @@
 #include "linkextractor.h"
 #include <QtCore>
-
+#include <QWebElementCollection>
+#include <QWebElement>
+#include <QWebFrame>
 
 LinkExtractor::LinkExtractor(QObject *parent) :
     QObject(parent)
 {
+    mp_web_view = new QWebView();
+}
+
+
+LinkExtractor::~LinkExtractor()
+{
+    delete mp_web_view;
 }
 
 
@@ -21,26 +30,33 @@ QList<ResourceLink> LinkExtractor::extract()
     // setMinimal(true) надо для разделения подряд идущих ссылок
     // если false то 2 и более подряд идущие ссылки воспримутся как одна
     //QRegExp reg_ex_a(QString::fromUtf8("<a.+href=[\"']([a-zA-Z:/%0-9&]*)[\"'].*>(.*)</a>"));
-    QRegExp reg_ex_a(QString::fromUtf8("<a.+href=[\"'](.*)[\"'].*>(.*)</a>"));
-    reg_ex_a.setMinimal(true);
+//    QRegExp reg_ex_a(QString::fromUtf8("<a.+href=[\"'](.*)[\"'].*>(.*)</a>"));
+//    reg_ex_a.setMinimal(true);
 
     // шаблон для ссылок записаных как текст
-    QRegExp reg_ex_arbitrary_link(QString::fromUtf8("((?:http|https)://[^\\s\\n\"'>]+)"));
+//    QRegExp reg_ex_arbitrary_link(QString::fromUtf8("((?:http|https)://[^\\s\\n\"'>]+)"));
 
 
     // поиск html ссылок
-    int pos = 0;
-    while ((pos = reg_ex_a.indexIn(m_text, pos)) != -1) {
-        addALink(reg_ex_a.cap(2), reg_ex_a.cap(1));
-        pos += reg_ex_a.matchedLength();
-    }
+//    int pos = 0;
+//    while ((pos = reg_ex_a.indexIn(m_text, pos)) != -1) {
+//        addALink(reg_ex_a.cap(2), reg_ex_a.cap(1));
+//        pos += reg_ex_a.matchedLength();
+//    }
 
 
     // поиск текстовых ссылок
-    pos = 0;
-    while ((pos = reg_ex_arbitrary_link.indexIn(m_text, pos)) != -1) {
-        addArbitraryLink(reg_ex_arbitrary_link.cap(1));
-        pos += reg_ex_arbitrary_link.matchedLength();
+//    pos = 0;
+//    while ((pos = reg_ex_arbitrary_link.indexIn(m_text, pos)) != -1) {
+//        addArbitraryLink(reg_ex_arbitrary_link.cap(1));
+//        pos += reg_ex_arbitrary_link.matchedLength();
+//    }
+
+
+    mp_web_view->setHtml(m_text);
+    QWebElementCollection elements = mp_web_view->page()->mainFrame()->findAllElements("a");
+    foreach (QWebElement e, elements) {
+        addALink(e.attribute(QString::fromUtf8("href")),e.attribute(QString::fromUtf8("href")));
     }
 
 
