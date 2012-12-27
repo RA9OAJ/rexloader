6d648c808dd01f7e21cc2d6f7af74e97852298ee
vedun.mail@gmail.com <vedun.mail@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2012-12-27 13:42:22 +0000

    issue 13 Добавил проверку доступности протокола.

--- trunk/REXLoader/linkextractor.h
+++ trunk/REXLoader/linkextractor.h
@@ -10,6 +10,7 @@ public:
     LinkType type;
     QString name;
     QString url;
+    QString protocol;
 };
 
 
@@ -22,7 +23,7 @@ public:
     void setText(const QString &text);
     QList<ResourceLink> extract();
 private:
-    QString m_text;
+    //QString m_text;
     QList<ResourceLink> m_link_list;
     QWebView *mp_web_view;
     // добавляем в список ссылок найденую ссылку A
