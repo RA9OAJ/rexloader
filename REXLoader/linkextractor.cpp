e4603db9227469419a0ea4aa69b3e2e0a5dd10a0
vedun.mail@gmail.com <vedun.mail@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2012-12-30 10:16:13 +0000

    Чистка от старых коментариев. Никаких изменений нет.

--- REXLoader/linkextractor.cpp
+++ REXLoader/linkextractor.cpp
@@ -19,7 +19,6 @@ LinkExtractor::~LinkExtractor()
 
 void LinkExtractor::setText(const QString &text)
 {
-    //m_text = text;
     mp_web_view->setHtml(text);
     m_link_list.clear();
 }
@@ -27,40 +26,10 @@ void LinkExtractor::setText(const QString &text)
 
 QList<ResourceLink> LinkExtractor::extract()
 {
-    // шаблон для html ссылок
-    // setMinimal(true) надо для разделения подряд идущих ссылок
-    // если false то 2 и более подряд идущие ссылки воспримутся как одна
-    //QRegExp reg_ex_a(QString::fromUtf8("<a.+href=[\"']([a-zA-Z:/%0-9&]*)[\"'].*>(.*)</a>"));
-//    QRegExp reg_ex_a(QString::fromUtf8("<a.+href=[\"'](.*)[\"'].*>(.*)</a>"));
-//    reg_ex_a.setMinimal(true);
-
-    // шаблон для ссылок записаных как текст
-//    QRegExp reg_ex_arbitrary_link(QString::fromUtf8("((?:http|https)://[^\\s\\n\"'>]+)"));
-
-
-    // поиск html ссылок
-//    int pos = 0;
-//    while ((pos = reg_ex_a.indexIn(m_text, pos)) != -1) {
-//        addALink(reg_ex_a.cap(2), reg_ex_a.cap(1));
-//        pos += reg_ex_a.matchedLength();
-//    }
-
-
-    // поиск текстовых ссылок
-//    pos = 0;
-//    while ((pos = reg_ex_arbitrary_link.indexIn(m_text, pos)) != -1) {
-//        addArbitraryLink(reg_ex_arbitrary_link.cap(1));
-//        pos += reg_ex_arbitrary_link.matchedLength();
-//    }
-
-
-    //mp_web_view->setHtml(m_text);
     QWebElementCollection elements = mp_web_view->page()->mainFrame()->findAllElements("a");
     foreach (QWebElement e, elements) {
         addALink(e.attribute(QString::fromUtf8("href")),e.attribute(QString::fromUtf8("href")));
     }
-
-
     return m_link_list;
 }
 
