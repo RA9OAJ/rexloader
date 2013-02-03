d7e3a13d02a03be7c51136fcd48413b609b8e3b6
vedun.mail@gmail.com <vedun.mail@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2013-02-03 14:35:11 +0000

    поменял интерфейс. Сделал для всех методов заглушки.

--- plugins/HashCalculator/HashCalculator.cpp
+++ plugins/HashCalculator/HashCalculator.cpp
@@ -4,8 +4,10 @@
 
 HashCalculator::HashCalculator()
 {
-    mp_control_dialog = 0;
-    mp_run_action = 0;
+    QAction *act = new QAction(QString::fromUtf8("запуск"),this);
+    m_run_action_list.append(act);
+
+    mp_control_dialog = new QDialog();
 }
 
 
@@ -14,18 +16,15 @@ HashCalculator::~HashCalculator()
 }
 
 
-void HashCalculator::setFileName(const QString &file_name)
+void HashCalculator::setFileName(const QStringList &file_name_list)
 {
-    m_file_name = file_name;
-    mp_control_dialog = new QDialog();
-    mp_run_action = new QAction(QString::fromUtf8("123"),this);
+    m_file_name_list = file_name_list;
 }
 
 
 void HashCalculator::release()
 {
     delete mp_control_dialog;
-    delete mp_run_action;
 }
 
 
@@ -37,22 +36,28 @@ QString HashCalculator::getLastError() const
 
 QStringList HashCalculator::pluginInfo() const
 {
-    QStringList list;
-
-    list << QString::fromUtf8("Модуль расчёта контрольных сумм");
-    return list;
+    QStringList pinfo;
+    pinfo << QString("Plugin: ") + tr("HashCalculator");
+    pinfo << QString("Authors: ") + tr("Alexey Schukin");
+    pinfo << QString("Place: Ukraine, Kiev, 2012-2013");
+    pinfo << QString("Build date: ") + QString("2013-02-03");
+    pinfo << QString("Version: ") + QString("0.1");
+    pinfo << QString("Contacts: mailto:mks-mail@ukr.net");
+    pinfo << QString("Lic: GNU/LGPL v2.1");
+    pinfo << QString("Description: ") + tr("Модуль для подсчёта контрольных сумм MD5 и SHA1");
+    return pinfo;
 }
 
 
-QAction *HashCalculator::getRunAction() const
+QList<QAction*> HashCalculator::getRunAction() const
 {
-    return mp_run_action;
+    return m_run_action_list;
 }
 
 
 void HashCalculator::run()
 {
-    mp_control_dialog->exec();
+    //mp_control_dialog->exec();
 }
 
 
