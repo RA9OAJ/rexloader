38f6233ae121e561a73fe69d6e4f57393fe67bae
vedun.mail@gmail.com <vedun.mail@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2013-02-23 16:08:54 +0000

    Закончил делать плагин подсчёта контрольных сумм

--- trunk/plugins/HashCalculator/HashCalculator.cpp
+++ trunk/plugins/HashCalculator/HashCalculator.cpp
@@ -2,9 +2,15 @@
 #include "HashCalculator.h"
 
 
-HashCalculator::HashCalculator()
+HashCalculator::HashCalculator():
+    mp_control_dialog(0)
 {
-    mp_control_dialog = new QDialog();
+    //mp_hash_calc = new HashCalculatorThread(this);
+
+    DataAction act;
+    act.act_id = 1;
+    act.act_title = tr("Подсчёт md5, sha1");
+    m_action_list.append(act);
 }
 
 
@@ -22,12 +28,39 @@ void HashCalculator::setFileName(const QStringList &file_name_list)
 void HashCalculator::release()
 {
     delete mp_control_dialog;
+    mp_control_dialog = 0;
+}
+
+
+QList<DataAction> HashCalculator::getActionList() const
+{
+    return m_action_list;
+}
+
+
+void HashCalculator::runAction(int act_id)
+{
+    if (mp_control_dialog == 0)
+    {
+        mp_control_dialog = new ControlDialog();
+    }
+    if (act_id == 1)
+    {
+        mp_control_dialog->setFileName(m_file_name_list.value(0));
+        mp_control_dialog->exec();
+    }
+}
+
+
+QTranslator *HashCalculator::getTranslator(const QLocale &locale)
+{
+    return 0;
 }
 
 
 QString HashCalculator::getLastError() const
 {
-    return QString::fromUtf8("");
+    return QString("");
 }
 
 
@@ -43,32 +76,6 @@ QStringList HashCalculator::pluginInfo() const
     pinfo << QString("Lic: GNU/LGPL v2.1");
     pinfo << QString("Description: ") + tr("Модуль для подсчёта контрольных сумм MD5 и SHA1");
     return pinfo;
-}
-
-QList<DataAction> HashCalculator::getActionList() const
-{
-    QList<DataAction> lst;
-    DataAction act;
-    act.act_id = 1;
-    act.act_title = tr("Подсчёт контрольных сумм");
-    //act.act_icon = QIcon(":/path/to/internal/icon"); // если есть жедание запихнуть через ресурсы иконку для пункта меню
-    lst.append(act);
-    return lst;
-}
-
-void HashCalculator::runAction(int act_id)
-{
-    // тут нужно обработать id и дернуть нужный метод
-}
-
-QTranslator *HashCalculator::getTranslator(const QLocale &locale)
-{
-    return 0;
-}
-
-void HashCalculator::run()
-{
-    //mp_control_dialog->exec();
 }
 
 
--- trunk/plugins/HashCalculator/controldialog.cpp
+++ trunk/plugins/HashCalculator/controldialog.cpp
@@ -1,14 +1,61 @@
 #include "controldialog.h"
 #include "ui_controldialog.h"
 
+
 ControlDialog::ControlDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ControlDialog)
 {
     ui->setupUi(this);
+    mp_hash_calc_thread = new HashCalculatorThread(this);
+    connect(mp_hash_calc_thread, SIGNAL(progress(int)), this, SLOT(percent(int)), Qt::QueuedConnection);
+    connect(mp_hash_calc_thread, SIGNAL(s_md5(QByteArray)), this, SLOT(md5_result(QByteArray)), Qt::QueuedConnection);
+    connect(mp_hash_calc_thread, SIGNAL(s_sha1(QByteArray)), this, SLOT(sha1_result(QByteArray)), Qt::QueuedConnection);
+    connect(ui->btnStop, SIGNAL(clicked()), this, SLOT(slotClose()));
 }
+
 
 ControlDialog::~ControlDialog()
 {
     delete ui;
+}
+
+
+void ControlDialog::setFileName(const QString &file_name)
+{
+    mp_hash_calc_thread->setFileName(file_name);
+}
+
+
+void ControlDialog::percent(int percent)
+{
+    ui->progressBar->setValue(percent);
+}
+
+
+int ControlDialog::exec()
+{
+    mp_hash_calc_thread->start();
+    return QDialog::exec();
+}
+
+
+void ControlDialog::md5_result(QByteArray result)
+{
+    ui->md5_result->setText(result);
+}
+
+
+void ControlDialog::sha1_result(QByteArray result)
+{
+    ui->sha1_result->setText(result);
+}
+
+
+void ControlDialog::slotClose()
+{
+    mp_hash_calc_thread->stop();
+    ui->md5_result->setText(QString(""));
+    ui->sha1_result->setText(QString(""));
+    reject();
 }
