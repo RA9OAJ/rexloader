b24aec5a4d080111a0023029a1808b1c520fa036
ra9oaj@gmail.com <ra9oaj@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2013-05-13 14:03:43 +0000

    Исправления:   * Приведение к стандарту GNU/GPL исходных кодов: указание лицензии и копирайты.

--- REXLoader/floating_window/graphwidget.h
+++ REXLoader/floating_window/graphwidget.h
@@ -1,13 +1,14 @@
 /*
-Project: REXLoader (Downloader), Source file: graphwidget.h
 Copyright (C) 2012-2013  Sarvaritdinov R.
 
-This program is free software: you can redistribute it and/or modify
+This file is part of REXLoader.
+
+REXLoader is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+REXLoader is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
