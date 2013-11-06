28662b257db25292d87ac44ed115e19b5f870fd3
ra9oaj@gmail.com <ra9oaj@gmail.com@3a10e0ea-d404-a8d6-8f5b-12e462dff962>
2013-11-06 14:58:38 +0000

    Реализована Digest аутентификация в классе Authentification.

--- plugins/HttpLoader/httploader.h
+++ plugins/HttpLoader/httploader.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QNetworkProxy>
 #include "httpsection.h"
 #include "../LoaderInterface.h"
+#include "authentification.h"
 
 class Task{
 public:
